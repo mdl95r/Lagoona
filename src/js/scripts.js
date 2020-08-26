69908cb8a8d6109da2430e5d14bcee0979e1321c
Artyom Bogomolov <awesomemdl95@gmail.com>
2020-08-26 22:29:10 +0300

    Готова основная структура сайта

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,4 +2,17 @@ document.addEventListener('DOMContentLoaded', function () {
     const lazyLoadInstance = new LazyLoad({
         elements_selector: ".lazy",
     });
+
+    document.addEventListener('click', function (e) {
+        const target = e.target;
+        if (target.dataset.itemValue) {
+            const { itemValue } = target.dataset;
+            target.parentNode.dataset.totalValue = itemValue;
+            sendInfoToBackend(itemValue);
+        }
+    });
+
+    function sendInfoToBackend(stars) {
+        console.log(stars);
+    }
 });
