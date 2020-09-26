99f406abd405120744cba6e993ba89bf537af051
Artyom Bogomolov <awesomemdl95@gmail.com>
2020-09-26 20:31:57 +0300

    Отменено действие по умолчанию по кнопке

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const burgerBtn = document.querySelector('.js-burger-btn');
     const menuList = document.querySelector('.menu__list');
+    const formBtn = document.querySelectorAll('.form-main__btn');
 
     const lazyLoadInstance = new LazyLoad({
         elements_selector: ".lazy",
@@ -43,4 +44,10 @@ document.addEventListener('DOMContentLoaded', function () {
             }, 300);
         }
     });
+
+    formBtn.forEach(function (btn) {
+        btn.addEventListener('click', function (e) {
+            e.preventDefault();
+        })
+    })
 });
